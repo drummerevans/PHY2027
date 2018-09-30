c59a91cf9485ad415919ffb0f417e383f9902b5e
drummerevans <drummerevans@live.co.uk>
2018-09-30 12:52:51 +0100

    Constants Added

--- PHY2027/Assessment1.c
+++ PHY2027/Assessment1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-#define GRAV 9.81
+#define GRAV 9.81 // creating the constants required for the calculations
+#define PI M_PI
 
 
 int main() {
-    float GRAVITY = 9.81;
-    float vel;
+    float GRAVITY = 9.81, vel;
 
     printf("Enter a valid velocity: ");
     scanf("%f", &vel);
@@ -14,8 +14,10 @@ int main() {
     printf("The acceleration due to gravity is: %.2f\n", GRAVITY);
     printf("%.2f\n", GRAV);
 
-    float power = pow(10, 2); // powe() returns the first argument raised to the power of the second
+    float power = pow(10, 2); // pow() returns the first argument raised to the power of the second
     printf("10 ^ 2 = %.0f\n", power);
+
+    printf("The value of pi is: %f", PI);
 
     return 0;
 }
