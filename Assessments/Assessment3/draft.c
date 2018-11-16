3c3e49a7dc415ee654d2644b7d04a60396f4f668
drummerevans <drummerevans@live.co.uk>
2018-11-16 15:34:59 +0000

    Assessment 3 Part Done

--- Assessments/Assessment3/draft.c
+++ Assessments/Assessment3/draft.c
@@ -12,21 +12,28 @@ The user input the name of the student to obtain the mark and grade for that par
 #include <string.h>
 #include <stdlib.h>
 
+#define NMAX 8
+
 typedef struct student {
     char name[50];
     char surname[50];
     int age;
     float mark[5]; // an array containing marks of five different assignments
-    char date[20];
+    float average_mark;
+    char grade;
 } Student;
 
-int indata();
+void indata(Student *sptr);
+void final_mark(Student *sptr);
+
 
 int main() {
-    
+    Student *struc_ptr = NULL;
+    struc_ptr = (Student *)malloc(NMAX * sizeof(Student)); // allocating 8 spaces for our structure array
 
-    char identity[20] = "Trundles";
+    char identity[20] = "Matt";
     char again[20];
+    
 
     printf("Input name: \n");
     scanf("%s", again);
@@ -39,60 +46,76 @@ int main() {
         printf("Student not found.\n");
     }
 
-    indata();
+    indata(struc_ptr);
+    final_mark(struc_ptr);
     
     return 0;
 }
 
-int indata() {
-    Student *sptr = NULL;
-    FILE *fptr;
-    sptr = (Student *)malloc(8 * sizeof(Student)); // allocated space for our structure array
+// indata reads data 
+void indata(Student *sptr) {
 
+    FILE *fptr;
     fptr = fopen("scores.txt", "r");
 
     if(fptr != NULL) {
-        // the while loop reads one line at a time from the text file "student_info.txt"
+        // the loop reads one line at a time from the text file "scores.txt"
         for(int i = 0; !feof(fptr); i++) {
             fscanf(fptr, "%s %s %d", sptr[i].name, sptr[i].surname, &sptr[i].age);
-            fscanf(fptr, "%f %f %f %f %f", &sptr[i].mark[0], &sptr[i].mark[1], &sptr[i].mark[2], &sptr[i].mark[3], &sptr[i].mark[4]);
-            // fscanf(fptr, "%s", sptr[i].date);
+
+            // the nested loop reads elements in from the text file into the 'mark' array in the structure
+            for(int j = 0; j < 5; j++) {
+                fscanf(fptr, "%f ", &sptr[i].mark[j]);
+            }
 
             fprintf(stdout, "%s %s %d\n", sptr[i].name, sptr[i].surname, sptr[i].age);
             fprintf(stdout, "%f %f %f %f %f\n", sptr[i].mark[0], sptr[i].mark[1], sptr[i].mark[2], sptr[i].mark[3], sptr[i].mark[4]);
-            // fprintf(stdout, "%s\n", sptr[i].date);
         }
         
-        // printf("Values stored in \'names\' array are: \n");
-        // for(int i = 0; i < 3; i++) {
-        //     printf("Name %d is %s\n", i, names[i]);
-        // }
-
-        float sum[20];
-
-        // for(int i = 0; i < arr_size; i++) {
-        //     if(i < 5) {
-        //         if(i == 0) {
-        //             //
-        //         }
-        //     }
-
-        //     fprintf(stdout, "%d %f\n", age[i], marks[i]);
-        // }
-
-
-        
-     
-        // for(int i = 0; i < 5; i++) {
-        //     printf("Marks: %f\n", marks[i]);
-        // }
         free(sptr);
         fclose(fptr);
-        return 0;
+
         }
 
+        // exits the program if the text file if there is a problem
         else {
             printf("Unable to open text file\n");
-            return 1;
+            exit(1); 
         }
     }
+
+void final_mark(Student *sptr) {
+    float sum, average;
+    for(int i = 0; i < NMAX; i++) {
+        printf("Student %d marks are: \n", i);
+        for(int j = 0; j < 5; j++) {
+            printf("The student mark is: %f\n", sptr[i].mark[j]);
+            sum += sptr[i].mark[j];
+        }
+        average = sum / 5.0;
+        sptr[i].average_mark = average;
+        printf("The average of student %d is %f\n", i, sptr[i].average_mark);
+
+    if(sptr[i].average_mark >= 70.0) {
+        sptr[i].grade = 'A';
+    }
+    else if(sptr[i].average_mark >= 60.0) {
+        sptr[i].grade = 'B';
+    }
+    else if(sptr[i].average_mark >= 50.0) {
+        sptr[i].grade = 'C';
+    }
+    else if(sptr[i].average_mark >= 40.0) {
+        sptr[i].grade = 'D';
+    }
+    else if(sptr[i].average_mark >= 30.0) {
+        sptr[i].grade = 'E';
+    }
+    else {
+        sptr[i].grade = 'F';
+    }
+    printf("Student %d grade is %c\n", i, sptr[i].grade);
+    sum = 0;
+    average = 0;
+    }
+}
